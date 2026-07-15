09cee33da8646f25a0e056740b4bea98ddacf2a9
Mekatrol <mekatrol@mekatrol.com>
2026-07-15 21:24:54 +1000

    Updated lint rules

--- frontend/flow-control-ui/eslint.config.ts
+++ frontend/flow-control-ui/eslint.config.ts
@@ -1,3 +1,5 @@
+import process from 'node:process'
+
 import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
@@ -35,4 +37,63 @@ export default defineConfigWithVueTs(
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
   skipFormatting,
+
+  {
+    rules: {
+      'prefer-promise-reject-errors': 'error',
+      'max-len': [
+        'error',
+        200,
+        {
+          ignorePattern: '^\\s*\\/',
+        },
+      ],
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false,
+        },
+      ],
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-console': [
+        process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        { allow: ['warn', 'error'] },
+      ],
+      'no-unused-vars': 'off',
+      'no-var': 'error',
+      'prefer-arrow-callback': 'error',
+      'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_.*$',
+          varsIgnorePattern: '^_$',
+          caughtErrorsIgnorePattern: '^_$',
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+          allowHigherOrderFunctions: true,
+          allowTypedFunctionExpressions: true,
+        },
+      ],
+      'vue/html-closing-bracket-newline': 'off',
+      'vue/first-attribute-linebreak': 'off',
+      'vue/html-indent': 'off',
+      'vue/html-self-closing': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/block-order': [
+        'error',
+        {
+          order: ['template', 'script', 'style'],
+        },
+      ],
+    },
+  },
 )
