c59a6b1f5c06f6da268dccbeb2caf8b1659509eb
Mekatrol <mekatrol@mekatrol.com>
2026-07-25 17:09:50 +1000

    Local app settings

--- backend/Server/Server.Api/Program.cs
+++ backend/Server/Server.Api/Program.cs
@@ -7,9 +7,16 @@ namespace Server.Api;
 
 public partial class Program
 {
+    private const string LocalSettingsFileName = "appsettings.Local.json";
+
     private static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Configuration.AddJsonFile(
+            LocalSettingsFileName,
+            optional: true,
+            reloadOnChange: true);
+
         var serverOptions = builder.Configuration.Get<ServerOptions>();
         if (builder.Configuration.GetSection(ServerOptions.AddressConfigurationKey).Exists()
             && serverOptions is not null)
